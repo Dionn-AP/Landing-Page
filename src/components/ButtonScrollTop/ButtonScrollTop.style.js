fd48be814435b5584226d72e7408cad1de12b292
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-06-28 10:16:17 -0300

    hover do botao scroll top

--- src/components/ButtonScrollTop/ButtonScrollTop.style.js
+++ src/components/ButtonScrollTop/ButtonScrollTop.style.js
@@ -1,17 +1,22 @@
 import styled from "styled-components";
 
-export const WrapperButtonScroll = styled.div`
+export const WrapperButtonScroll = styled.a`
     display: flex;
     align-items: center;
     justify-content: center;
     height: 6rem;
     width: 6rem;
     border-radius: 50%;
-    background-color: ${ props => `var(${props.colorButtonScroll})`};
+    background-color: ${props => `var(${props.colorButtonScroll})`};
     cursor: pointer;
-    position: fixed;
+    position: absolute;
     bottom: 12.5rem;
     right: 5rem;
+
+    :hover {
+        opacity: 0.9;
+        transition: all 0.3s;
+    }
 `
 
 export const ImageArrowUpScroll = styled.img`
