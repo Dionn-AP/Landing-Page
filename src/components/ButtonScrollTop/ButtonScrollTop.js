6d10ec9606d963226751920aa20de18f41debaa9
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-06-28 10:14:17 -0300

    configurando o scrollReveal

--- src/components/ButtonScrollTop/ButtonScrollTop.js
+++ src/components/ButtonScrollTop/ButtonScrollTop.js
@@ -1,10 +1,23 @@
 import { WrapperButtonScroll, ImageArrowUpScroll } from './ButtonScrollTop.style';
+import { useEffect } from 'react';
+import ScrollReveal from 'scrollreveal';
 import ImageArrowUp from '../../assets/arrow-up-scroll.svg';
 
 export default function ButtonScrollTop({ colorButtonBack }) {
+    useEffect(() => {
+        const srScroll = ScrollReveal({
+            distance: '100px',
+            duration: 1000,
+            reset: true
+        });
+        srScroll.reveal('#scroll', { delay: 100, origin: 'bottom' });
+    }, []);
 
     return (
-        <WrapperButtonScroll colorButtonScroll={colorButtonBack}>
+        <WrapperButtonScroll
+            id='scroll'
+            onClick={() => window.scroll({top: 0, behavior: 'smooth'})}
+            colorButtonScroll={colorButtonBack}>
             <ImageArrowUpScroll src={ImageArrowUp} alt='arrow up' />
         </WrapperButtonScroll>
     )
