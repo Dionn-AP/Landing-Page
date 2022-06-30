bf1ff34d5951fa9455347ae43b4e7e6fa0810b90
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-06-30 06:22:50 -0300

    responsividade do Footer

--- src/components/Footer/Footer.style.js
+++ src/components/Footer/Footer.style.js
@@ -10,6 +10,26 @@ export const WrapperFooter = styled.footer`
     position: absolute;
     bottom: 0;
     background-color: var(--color-primery-grayish-dark-blue);
+
+    @media (max-width: 1389px) {
+        padding: 2.4rem 12rem;
+    }
+
+    @media (max-width: 1264px) {
+        padding: 2.4rem 8rem;
+    }
+
+    @media (max-width: 1064px) {
+        padding: 2.4rem 6rem
+    }
+
+    @media (max-width: 768px) {
+        padding: 2.4rem 3rem
+    }
+
+    @media (max-width: 575px) {
+        flex-direction: column;
+    }
 `
 
 export const WrapperNavPages = styled.nav`
@@ -18,6 +38,35 @@ export const WrapperNavPages = styled.nav`
     align-items: center;
     justify-content: space-between;
     width: 40%;
+
+    @media (max-width: 1264px) {
+        justify-content: flex-start;
+        width: 50%;
+    }
+
+    @media (max-width: 860px) {
+        width: 60%;
+    }
+
+    @media (max-width: 760px) {
+        width: 65%;
+    }
+
+    @media (max-width: 680px) {
+        width: 68%;
+    }
+
+    @media (max-width: 575px) {
+        width: 95%;
+        justify-content: space-between;
+        margin-bottom: 3rem;
+    }
+
+    @media (max-width: 500px) {
+        img {
+            display: none;
+        }
+    }
 `
 
 export const WrapperMenuFooter = styled.div`
@@ -28,11 +77,17 @@ export const WrapperMenuFooter = styled.div`
     width: 35rem;
     letter-spacing: 0.2rem;
     justify-content: space-between;
+    margin-left: 1.5rem;
     color: var(--color-primery-grayish-dark-blue);
 
     a:hover {
         color: var(--color-primary-cyan);
         transition: all 0.4s;
+    }
+
+    @media (max-width: 500px) {
+        margin-left: 0;
+        width: 100%;
     }
 `
 
@@ -42,6 +97,23 @@ export const WrapperNavSocialNetwork = styled.nav`
     align-items: center;
     justify-content: space-between;
     width: 15%;
+
+    @media (max-width: 1264px) {
+        width: 20%;
+    }
+
+    @media (max-width: 760px) {
+        width: 25%;
+    }
+
+    @media (max-width: 575px) {
+        width: 80%;
+    }
+
+    @media (max-width: 500px) {
+        width: 90%;
+        margin-right: 1.3rem;
+    }
 `
 
 export const ImageSocialNetwork = styled.img`
